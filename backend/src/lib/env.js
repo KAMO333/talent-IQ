8e4d2e5ea93310a4fbe4c4059ebd5c9ce902be85
KAMO333 <mmopanemoroka@gmail.com>
2026-02-04 16:00:11 +0200

    feat: refactor Inngest integration by moving functions to a new file and updating environment variable names

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -6,8 +6,8 @@ export const ENV = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
     DB_URL: process.env.DB_URL,
-    INJECT_EVENT_KEY: process.env.INJECT_EVENT_KEY,
-    INJECT_SIGN_IN_KEY: process.env.INJECT_SIGN_IN_KEY,
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGN_IN_KEY: process.env.INNGEST_SIGN_IN_KEY,
     STREAM_API_KEY: process.env.STREAM_API_KEY,
     STREAM_API_SECRET: process.env.STREAM_API_SECRET,
     CLIENT_URL: process.env.CLIENT_URL
