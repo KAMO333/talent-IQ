2565080b7ba14661f60836c810cafc17c7fa9286
KAMO333 <mmopanemoroka@gmail.com>
2026-02-13 14:37:22 +0200

    fix: update root build script for Render deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,47 +1,48 @@
-import express from "express";
-import path from "path";
-import cors from "cors";
-import { serve } from "inngest/express";
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import { serve } from 'inngest/express';
+import { ENV } from './lib/env.js';
+import { connectDB } from './lib/db.js';
+import { functions, inngest } from './lib/inngest.js';
 
-import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
-import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
 
-// middleware
+// middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cors({origin: ENV.CLIENT_URL, credentials: true}));
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/inngest", serve({client:inngest, functions}))
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ msg: "api is up and running" });
-});
 
-app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "this is the books endpoint" });
-});
+app.get('/health', (req, res) => {
+    res.status(200).json({ message: 'this is the health endpoint...' });
+})
+
+app.get('/books', (req, res) => {
+    res.status(200).json({ message: 'this is the books endpoint...' });
+})
 
 // make our app ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
 }
 
 const startServer = async () => {
-  try {
-    await connectDB();
-    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
-  } catch (error) {
-    console.error("💥 Error starting the server", error);
-  }
-};
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => console.log(`Server is running on port ${ENV.PORT}`));
+    } catch (error) {
+        console.error("❌ Failed to start server:", error);
+    }
+}
 
 startServer();
